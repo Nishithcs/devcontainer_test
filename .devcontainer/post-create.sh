bc8da6304568ef36d873beb9fde0aaab5e6df605
nishith.salian <nishith.salian@innoscripta.com>
2025-08-25 14:00:47 +0530

    first commit

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "-----------------------------------------"
+echo "Dev Container successfully created!"
+echo "-----------------------------------------"
+echo ""
+echo "Your Flask app is ready to run."
+echo "To start the server, open a new terminal (Ctrl+Shift+\`) and run:"
+echo "python /workspace/app.py"
+echo ""
+echo "A Redis database is also running and available at: redis://db:6379"
+echo ""
